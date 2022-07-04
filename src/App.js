be0e970a388da8d35002de3365fc28cd6c04f701
amir javadi <amirjavadi1386js@gmail.com>
2022-07-04 00:20:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import Menu from './component/Menu'  
+import Menu from './component/Menu'    
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import {Button,card, CardImg} from 'react-bootstrap'
 export default class App extends Component {
